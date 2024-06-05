2deea856d8e7212927cf75fb96ca7a20b6410534
Cavecake <simon.maxton@web.de>
2024-06-05 21:57:08 +0200

    Update GravityFormulars.py
    Added two other helper functions

--- DataTypes/GravityFormulars.py
+++ DataTypes/GravityFormulars.py
@@ -1,8 +1,10 @@
 from DataTypes.constant import Constants
 import numpy as np
 from scipy.spatial import distance
+import math
 
 G = Constants(6.67259,0.00085,-11)
+C = 299792458
 
 def calc_force(mass1, mass2, distance):
     force = G * (mass1 * mass2)/(distance * distance)
@@ -19,3 +21,6 @@ def check_collision(point1, point2, radius1, radius2):
     if calc_distance(point1,point2) <= radius1 + radius2:
         return True
     return False
+
+def magnitude(vector): 
+    return math.sqrt(sum(pow(element, 2) for element in vector))
